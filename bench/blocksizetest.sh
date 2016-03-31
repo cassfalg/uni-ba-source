8930e86ce950ce81099f5a9f9f0e0d18d21bfaa8
Christian Aßfalg <christian.assfalg@uni-ulm.de>
2016-03-31 14:52:53 +0000

    make starting block sizes slightly bigger - that small makes little sense and is very slow on the odroid

--- bench/blocksizetest.sh
+++ bench/blocksizetest.sh
@@ -1,13 +1,16 @@
 #/bin/bash
 
 make header
-for i in 1 2 3 4 5 6 7 8
+#for i in 1 2 3 4 5 6 7 8
+for i in 2 3 4 5 6 7 8
 do
     export D_BLOCKSIZE_MR=$i
-    for j in 1 2 3 4 5 6 7 8
+#    for j in 1 2 3 4 5 6 7 8
+    for j in 2 3 4 5 6 7 8
     do
         export D_BLOCKSIZE_NR=$j
-        for k in `seq 1 32` 40 48 56 64 72 80 88 96
+#        for k in `seq 1 32` 40 48 56 64 72 80 88 96
+        for k in `seq 4 32` 40 48 56 64 72 80 88 96
         do
             export D_BLOCKSIZE_MC=$(($i*$k))
             export D_BLOCKSIZE_KC=$(($j*$k))
