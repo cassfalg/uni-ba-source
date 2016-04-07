3d9893e1b81c41081c85024fc5ce3397b3f888c8
Christian Aßfalg <christian.assfalg@uni-ulm.de>
2016-04-07 12:18:36 +0000

    added performance results for atlas, blis and openblas libraries, as well as a run of blocksizetest.sh

--- bench/blocksizetest.sh
+++ bench/blocksizetest.sh
@@ -10,12 +10,12 @@ do
     do
         export D_BLOCKSIZE_NR=$j
 #        for k in `seq 1 32` 40 48 56 64 72 80 88 96
-        for k in `seq 4 32` 40 48 56 64 72 80 88 96
+        for k in `seq 4 32` 40 48 56 64 72 80 88 96 128 196 256
         do
             export D_BLOCKSIZE_MC=$(($i*$k))
             export D_BLOCKSIZE_KC=$(($j*$k))
             export D_BLOCKSIZE_NC=$((64*$k))
-            echo $D_BLOCKSIZE_MR $D_BLOCKSIZE_NR $D_BLOCKSIZE_MC $D_BLOCKSIZE_KC $D_BLOCKSIZE_NC
+#            echo $D_BLOCKSIZE_MR $D_BLOCKSIZE_NR $D_BLOCKSIZE_MC $D_BLOCKSIZE_KC $D_BLOCKSIZE_NC
             export BLOCKDEFS="-DD_BLOCKSIZE_MR=$i -DD_BLOCKSIZE_NR=$j -DD_BLOCKSIZE_MC=$(($i*$k)) -DD_BLOCKSIZE_KC=$(($j*$k)) -DD_BLOCKSIZE_NC=$((64*$k))"
             make runperf
         done
